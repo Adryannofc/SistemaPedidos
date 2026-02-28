be56cbe4c59cb2ea7c2bce28ff18c111683d5872
Guilherme R.M <emaildoguilherme16@gmail.com>
2026-02-28 18:56:53 -0300

    feat: update PedidoRepository + add PedidoRepositoryMemoria

--- src/main/java/com/pedidos/domain/repository/PedidoRepository.java
+++ src/main/java/com/pedidos/domain/repository/PedidoRepository.java
@@ -1,18 +1,29 @@
 package com.pedidos.domain.repository;
 
+import com.pedidos.domain.enums.StatusPedido;
 import com.pedidos.domain.model.Usuario;
 import com.pedidos.domain.model.Pedido;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface PedidoRepository {
 
     void salvar(Pedido pedido);
 
-    Optional<Pedido> buscarPorId(Long id);
+    Optional<Pedido> buscarPorId(UUID id);
 
     List<Pedido> listarTodos();
 
-    void deletar(Long id);
+    List<Pedido> buscarPorCliente(UUID clienteId);
+
+    List<Pedido> buscarPorRestaurante(UUID restauranteId);
+
+    List<Pedido> listarAtivosPorRestaurante(UUID restauranteId);
+
+    List<Pedido> filtrarPorStatus(UUID restauranteId, StatusPedido status);
+
+    void deletar(UUID id);
+
 
 }
